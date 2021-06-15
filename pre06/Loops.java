a287f2879ef8d135091478a5a5093715f541d5a5
tyl3452 <tayou.liu@gmail.com>
2021-06-15 17:27:49 -0400

    loopy loops

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -5,14 +5,23 @@ public class Loops{
 		System.out.println("Eric Liu pre06");
 
 		//test power
-		System.out.println(power(2,10));
+		System.out.println("Test power");
+		System.out.println(power(2,16));
 		System.out.println(power(98,0));
 		System.out.println(power(23,1));
 		
 		//test factorial
-		
-		
-		
+		System.out.println("Test factorial");
+		System.out.println(factorial(3));
+		System.out.println(factorial(5));
+		System.out.println(factorial(12));
+
+		//test myexp
+		System.out.println("Test myexp");
+		System.out.println(myexp(1,1));
+		System.out.println(myexp(1,2));
+		System.out.println(myexp(1,6));
+
 	}//end main
 	
 	/* @desc exercise 3
@@ -24,23 +33,12 @@ public class Loops{
 	 */
 	public static double power(double x, int n) {
 		//iterative power
-		/*
-		if(n == 0) {
-			return 1;
-		} else {
-			double total = 1;
-			for(int i = 0; i < n; i++) {
-				total += x;
-			}//end for
-			return(total);
-		}
-		*/
 		double total = 1;
 		for(int i = 0; i < n; i++) {
 			total *= x;
 		}//end for
 		
-		
+		return (total);
 		
 	}//end power
 	
@@ -51,21 +49,33 @@ public class Loops{
 	 * @param integer n is the power
 	 * @return double of the resulting expression
 	 */
-	public static double factorial(double x, int n) {
-		//iterative power
-		if(n == 0) {
-			return 1;
-		} else {
-			double total = 0;
-			for(int i = 0; i < n; i++) {
-				total += x;
-			}//end for
-			return(total);
-		}
+	public static int factorial(int n) {
+		int result = 1;
+		for(int i = n;i > 0; i--) {
+			result *= i;
+		}//end for 
+		return (result);
 	}//end factorial
 	
 	
-	
+	/* @desc exercise 5
+	 * myexp
+	 *
+	 * @param integer x is the power
+	 * @param integer n is number of terms
+	 * @return double of the resulting expression
+	 */
+	public static double myexp(int x, int n) {
+		//iterative myexp
+		double result = 0;
+		
+		for(int i = 0; i < n; i++) {
+			result += power(x,i+1) / factorial(i+1);
+		}//end for
+
+		return (result);
+
+	}//end myexp
 	
 	
 	
