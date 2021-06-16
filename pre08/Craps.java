de58547ffb02ad73fdb1f87b1e0534413ef4b62e
tyl3452 <tayou.liu@gmail.com>
2021-06-15 20:18:09 -0400

    shooting my shot

---
+++ pre08/Craps.java
@@ -0,0 +1,47 @@
+
+public class Craps{
+	
+	/* @desc Choose random number from 1 to n
+	 * 
+	 * @param int n max number to choose random number
+	 * @return int between 1 and the number n
+	 */
+	public static int roll(int n) {
+		
+		
+		
+	}//end roll
+	
+	
+	/* @desc Simulates rolling dice
+	 * 
+	 * @param int numDice number of Dice to use
+	 * @param int maxValue max value of the dice
+	 * @return int sum result of rolling the dice
+	 */
+	public static int shoot(int numDice, int maxValue) {
+		
+		
+		
+	}//end shoot
+	
+	
+	/* @desc checks win or lose
+	 * 
+	 * @return boolean if shooter wins or loses
+	 */
+	public static int round(int numDice, int maxValue) {
+		
+		
+		
+	}//end round
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Eric Liu pre08");
+
+		//test powArray
+		
+	}//end main
+}//end class
